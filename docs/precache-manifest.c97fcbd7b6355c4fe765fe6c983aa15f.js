d0e26dfa4dc515b2f22c0aab6e1665f71e2059f1
Alejandra <aledev.roman@gmail.com>
2020-08-23 19:59:01 -0500

    Docs agregado

---
+++ docs/precache-manifest.c97fcbd7b6355c4fe765fe6c983aa15f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20cc019499da61a88f819e69c04dd93e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "730cce4ff6797ac9c9f3",
+    "url": "/static/css/main.0feff4c7.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "730cce4ff6797ac9c9f3",
+    "url": "/static/js/main.7bd183c5.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
